8c609e87ed5e134bb001ac665877cd5cc7fd352d
KSTA <>
2025-07-24 15:56:22 +0630

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_basic_examples
+
+A new Flutter project.
---
+++ lib/basic_widgets.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              MyStatelessWidget(),
+              MyStatefulWidget(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    print('Building MyStatelessWidget');
+    return const Text('This widget never changes');
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  String _text = 'Initial text';
+
+  void _updateText() {
+    setState(() {
+      _text = 'Updated text';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('Building MyStatefulWidget');
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(_text),
+        ElevatedButton(
+          onPressed: _updateText,
+          child: const Text('Update'),
+        ),
+      ],
+    );
+  }
+}
