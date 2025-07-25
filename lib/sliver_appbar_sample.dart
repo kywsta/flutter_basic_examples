e868c6684807fab44048f9dedb621b7a973674e5
KSTA <>
2025-07-25 12:55:32 +0630

    Add sliver list view sample and make sliver appbar sample cleaner

--- lib/sliver_appbar_sample.dart
+++ lib/sliver_appbar_sample.dart
@@ -15,9 +15,6 @@ class BasicSliverAppBarExample extends StatelessWidget {
           SliverAppBar(
             title: const Text('Basic SliverAppBar'),
             expandedHeight: 200,
-            floating: false,
-            pinned: false,
-            snap: false,
             flexibleSpace: FlexibleSpaceBar(
               background: Container(
                 decoration: const BoxDecoration(
@@ -42,30 +39,16 @@ class BasicSliverAppBarExample extends StatelessWidget {
               ),
             ),
           ),
-          SliverGrid(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 4,
-              mainAxisSpacing: 10.0,
-              crossAxisSpacing: 10.0,
-              childAspectRatio: 4 / 3,
+          SliverSafeArea(
+            sliver: SliverList(
+              delegate: SliverChildBuilderDelegate((context, index) {
+                debugPrint('Building list item $index');
+                return ListTile(
+                  title: Text('Item $index'),
+                  subtitle: Text('Subtitle for item $index'),
+                );
+              }, childCount: 50),
             ),
-            delegate: SliverChildBuilderDelegate((context, index) {
-              debugPrint('Building grid item $index');
-              return Container(
-                alignment: Alignment.center,
-                color: Colors.teal[100 * (index % 9)],
-                child: Text('grid item $index'),
-              );
-            }, childCount: 15),
-          ),
-          SliverList(
-            delegate: SliverChildBuilderDelegate((context, index) {
-              debugPrint('Building list item $index');
-              return ListTile(
-                title: Text('Item $index'),
-                subtitle: Text('Subtitle for item $index'),
-              );
-            }, childCount: 50),
           ),
         ],
       ),
---
+++ lib/sliver_list_view_sample.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MaterialApp(home: BasicSliverAppBarExample()));
+}
+
+class BasicSliverAppBarExample extends StatelessWidget {
+  const BasicSliverAppBarExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: const Text('Basic SliverAppBar'),
+            expandedHeight: 200,
+            floating: true,
+            pinned: true,
+            snap: false,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [Colors.blue, Colors.purple],
+                  ),
+                ),
+                child: Center(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.white,
+                      child: Icon(Icons.person),
+                    ),
+                    title: Text('Flexible Space'),
+                    subtitle: Text('Subtitle for Flexible Space'),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SliverSafeArea(
+            sliver: SliverGrid(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 4,
+                mainAxisSpacing: 10.0,
+                crossAxisSpacing: 10.0,
+                childAspectRatio: 4 / 3,
+              ),
+              delegate: SliverChildBuilderDelegate((context, index) {
+                debugPrint('Building grid item $index');
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.teal[100 * (index % 9)],
+                  child: Text('grid item $index'),
+                );
+              }, childCount: 15),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate((context, index) {
+              debugPrint('Building list item $index');
+              return ListTile(
+                title: Text('Item $index'),
+                subtitle: Text('Subtitle for item $index'),
+              );
+            }, childCount: 50),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.navigate_next),
+      ),
+    );
+  }
+}
